bef735297303be9ba18907a25e489b9eab3f4ee1
Vincent Nguyen <ngn.vincent@gmail.com>
2023-05-17 04:08:49 -0700

    Add project files.

---
+++ ConfigJson.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discordBot {
+	public struct ConfigJson {
+		[JsonProperty("token")]
+		public string Token { get; private set; }
+		[JsonProperty("prefix")]
+		public string Prefix { get; private set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace discordBot {
+	class Program {
+		static void Main(string[] args) {
+			var bot = new Bot();
+			bot.RunAsync().GetAwaiter().GetResult();
+		}
+	}
+}
