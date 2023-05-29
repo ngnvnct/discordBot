9331b60461662e396e194d2e7342c0f3e69d927e
Vincent Nguyen <ngn.vincent@gmail.com>
2023-05-29 01:27:23 -0700

    moved to all slash commands

--- Bot.cs
+++ Bot.cs
@@ -17,105 +17,82 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace discordBot {
-	public class Bot {
-		public DiscordClient ?Client { get; private set; }
-		public InteractivityExtension ?Interactivity { get; private set; }
-		public CommandsNextExtension ?Commands { get; private set; }
-		public SlashCommandsExtension ?SlashCommands { get; private set; }
+    public class Bot {
+        public DiscordClient ?Client { get; private set; }
+        public InteractivityExtension ?Interactivity { get; private set; }
+        public CommandsNextExtension ?Commands { get; private set; }
+        public SlashCommandsExtension ?SlashCommands { get; private set; }
 
-		public async Task RunAsync() {
-			// sets up our api in a way that prevents hardcoding the api-key by using a file reading system
-			var json = string.Empty;
-			using(var fs = File.OpenRead("config.json"))
-			using(var sr = new StreamReader(fs, new UTF8Encoding(false)))
-				json = await sr.ReadToEndAsync();
+        public async Task RunAsync() {
+            // sets up our api in a way that prevents hardcoding the api-key by using a file reading system
+            // coulda also used user secrets but whateva
+            var json = string.Empty;
+            using(var fs = File.OpenRead("config.json"))
+            using(var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
 
-			var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
-			
-			// creating conditions of our new bot client instance
-			var config = new DiscordConfiguration() {
-				Intents = DiscordIntents.All,
-				Token = configJson.Token,
-				TokenType = TokenType.Bot,
-				AutoReconnect = true,
-			};
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            
+            // creating conditions of our new bot client instance
+            var config = new DiscordConfiguration() {
+                Intents = DiscordIntents.All,
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
 
-			Client = new DiscordClient(config);
-			Client.UseInteractivity(new InteractivityConfiguration() {
-				Timeout = TimeSpan.FromMinutes(2)
-			});
+            Client = new DiscordClient(config);
+            Client.UseInteractivity(new InteractivityConfiguration() {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
 
-			var commandsConfig = new CommandsNextConfiguration() {
-				StringPrefixes = new string[] { configJson.Prefix },
-				EnableMentionPrefix = true,
-				EnableDms = true,
-				EnableDefaultHelp = true,
-				UseDefaultCommandHandler = false
-			};
+            var commandsConfig = new CommandsNextConfiguration() {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = true,
+                UseDefaultCommandHandler = true
+            };
 
 
-			// register commands
-			Commands = Client.UseCommandsNext(commandsConfig);
-			Commands.RegisterCommands<RealCommands>();
-			Commands.RegisterCommands<TestCommands>();
+            // register commands
+            SlashCommands = Client.UseSlashCommands();
+            SlashCommands.RegisterCommands<SlashCommands>(configJson.GuildID);
 
-			SlashCommands = Client.UseSlashCommands();
-			SlashCommands.RegisterCommands<SlashCommands>(configJson.GuildID);
-
-			// error handling
-			Commands.CommandErrored += OnCommandError;
+            // error handling
             SlashCommands.SlashCommandErrored += OnSlashCommandError;
 
-			Client.Ready += OnClientReady;
+            Client.Ready += OnClientReady;
 
-			await Client.ConnectAsync();
-			await Task.Delay(-1);
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
 
-		}
+        }
 
 
         private async Task OnSlashCommandError(SlashCommandsExtension sender, DSharpPlus.SlashCommands.EventArgs.SlashCommandErrorEventArgs e) {
             if (e.Exception is SlashExecutionChecksFailedException) {
-				var castedException = (SlashExecutionChecksFailedException)e.Exception;
-				string cooldownTimer = string.Empty;
+                var castedException = (SlashExecutionChecksFailedException)e.Exception;
+                string cooldownTimer = string.Empty;
 
-				foreach (var check in castedException.FailedChecks) {
-					var cooldown = (SlashCooldownAttribute)check;
-					TimeSpan timeLeft = cooldown.GetRemainingCooldown(e.Context);
-					cooldownTimer = timeLeft.ToString(@"hh\:mm\:ss");
-				}
-				var cooldownMessage = new DiscordInteractionResponseBuilder()
-											.AddEmbed(new DiscordEmbedBuilder()
-											.WithColor(DiscordColor.Red)
-											.WithTitle("Max amount of uses for this command, please wait for the cooldown to end.")
-											.WithDescription(cooldownTimer)
-											);
-				await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, cooldownMessage);
-			}
-        }
+                foreach (var check in castedException.FailedChecks) {
+                    var cooldown = (SlashCooldownAttribute)check;
+                    TimeSpan timeLeft = cooldown.GetRemainingCooldown(e.Context);
+                    cooldownTimer = timeLeft.ToString(@"hh\:mm\:ss");
+                }
 
-
-        private async Task OnCommandError(CommandsNextExtension sender, CommandErrorEventArgs e) {
-            if (e.Exception is ChecksFailedException) {
-				var castedException = (ChecksFailedException)e.Exception;
-				string cooldownTimer = string.Empty;
-
-				foreach (var check in castedException.FailedChecks) {
-					var cooldown = (CooldownAttribute)check;
-					TimeSpan timeLeft = cooldown.GetRemainingCooldown(e.Context);
-					cooldownTimer = timeLeft.ToString(@"hh\:mm\:ss");
-				}
-				var cooldownMessage = new DiscordEmbedBuilder() {
-					Title = "Wait for the cooldown to end",
-					Description = cooldownTimer,
-					Color = DiscordColor.Red
-				};
-				await e.Context.Channel.SendMessageAsync(cooldownMessage);
-			}
+                var cooldownMessage = new DiscordInteractionResponseBuilder()
+                                            .AddEmbed(new DiscordEmbedBuilder() {
+                                                Color = DiscordColor.Red,
+                                                Title = "Max amount of uses for this command, please wait for the cooldown to end.",
+                                                Description = cooldownTimer
+                                            });
+                await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, cooldownMessage);
+            }
         }
 
         private Task OnClientReady(DiscordClient s, ReadyEventArgs e) {
-			return Task.CompletedTask;
-		}
-	}
+            return Task.CompletedTask;
+        }
+    }
 }
--- Commands/SlashCommands.cs
+++ Commands/SlashCommands.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.Attributes;
 using System;
@@ -9,25 +10,111 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace discordBot.Commands {
-	public class SlashCommands : ApplicationCommandModule {
-		[SlashCommand("test", "slash command for testing purposes wow")]
-		public async Task TestCommand(InteractionContext ctx) {
-			await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("hello bro"));
-		}
+    public class SlashCommands : ApplicationCommandModule {
+        [SlashCommand("repo", "Link to the LC solutions github repo.")]
+        [SlashCooldown(1, 10, SlashCooldownBucketType.User)]
+        public async Task GetRepoLink(InteractionContext ctx) {
+            var linkMessage = new DiscordInteractionResponseBuilder()
+                                .AddEmbed(new DiscordEmbedBuilder()
 
-		[SlashCommand("repo", "Link to the LC solutions github repo.")]
-		[SlashCooldown(1, 10, SlashCooldownBucketType.User)]
-		public async Task GetRepoLink(InteractionContext ctx) {
-			var linkMessage = new DiscordInteractionResponseBuilder()
-								.AddEmbed(new DiscordEmbedBuilder()
+                                .WithColor(DiscordColor.Goldenrod)
+                                .WithTitle("LeetCode solutions repository")
+                                .WithAuthor("thuanle123")
+                                .WithDescription("Solutions written in Python, C#, and Java!\n https://github.com/thuanle123/Leetcode")
+                                );	
+                                
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, linkMessage);
+        }
 
-								.WithColor(DiscordColor.Goldenrod)
-								.WithTitle("LeetCode solutions repository")
-								.WithAuthor("thuanle123")
-								.WithDescription("Solutions written in Python, C#, and Java!\n https://github.com/thuanle123/Leetcode")
-								);	
-								
-			await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, linkMessage);
-		}
-	}
+        [SlashCommand("createpoll", "create a poll and let users vote")]
+        public async Task PollCommand(InteractionContext ctx, [Option("question", "poll subject")] string question,
+                                                                [Choice("60s", 60)][Choice("30s", 30)][Choice("15s", 15)]
+                                                                    [Option("timelimit", "the amount of time that the poll is active")] long timelimit,
+                                                                [Option("option1", "first option")] string option1,
+                                                                [Option("option2", "second option")] string option2,
+                                                                [Option("option3", "third option")] string option3,
+                                                                [Option("option4", "fourth option")] string option4) {
+
+            await ctx.DeferAsync();
+
+            var interactivity = ctx.Client.GetInteractivity();
+            int count1 = 0; 
+            int count2 = 0;	
+            int count3 = 0; 
+            int count4 = 0;
+            TimeSpan timer = TimeSpan.FromSeconds(timelimit);
+            string title = String.Join("", question);
+
+            DiscordEmoji[] optionsEmojis = {    DiscordEmoji.FromName(ctx.Client, ":one:", false),
+                                                DiscordEmoji.FromName(ctx.Client, ":two:", false), 
+                                                DiscordEmoji.FromName(ctx.Client, ":three:", false),
+                                                DiscordEmoji.FromName(ctx.Client, ":four:", false) };
+
+            string pollOptionsStrings = (   $"{optionsEmojis[0]} | {option1}\n" +
+                                            $"{optionsEmojis[1]} | {option2}\n" +
+                                            $"{optionsEmojis[2]} | {option3}\n" +
+                                            $"{optionsEmojis[3]} | {option4}"   );
+
+            var pollMessage = new DiscordEmbedBuilder() {
+                Color = DiscordColor.Sienna,
+                Title = title,
+                Description = pollOptionsStrings
+            };
+
+            var message = await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(pollMessage));
+
+            foreach( var emoji in optionsEmojis ) {
+                await message.CreateReactionAsync(emoji);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(message, timer);
+
+            foreach (var emoji in result) {
+                if (emoji.Emoji == optionsEmojis[0]) {
+                    count1++;
+                }
+                if (emoji.Emoji == optionsEmojis[1]) {
+                    count2++;
+                }
+                if (emoji.Emoji == optionsEmojis[2]) {
+                    count3++;
+                }
+                if (emoji.Emoji == optionsEmojis[3]) {
+                    count4++;
+                }
+            }
+
+            int totalVotes = count1 + count2 + count3 + count4;
+
+            string resultsStrings = (   $"{optionsEmojis[0]} | {count1} votes\n" +
+                                        $"{optionsEmojis[1]} | {count2} votes\n" +
+                                        $"{optionsEmojis[2]} | {count3} votes\n" +
+                                        $"{optionsEmojis[3]} | {count4} votes\n\n" +
+                                        $"Total votes: {totalVotes}"    );
+            var resultMessage = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder() {
+                    Color = DiscordColor.Green,
+                    Title = "Result of poll",
+                    Description = resultsStrings
+                });
+
+            await ctx.Channel.SendMessageAsync(resultMessage);
+
+        }
+        
+        [SlashCommand("test", "test slash command using whatever the fk")]
+        public async Task TestCommand(InteractionContext ctx,   [Choice("variable", "value")]
+                                                                [Option("displayName", "description of what to input")] string text) {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                                                                                                .WithContent("test"));
+
+            var embedMessage = new DiscordEmbedBuilder() {
+                Color = DiscordColor.Chartreuse,
+                Title = "test",
+                Description = text
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: embedMessage);
+        }
+    }
 }
